9c86c4ee0ad9d32e234994bfec40ffcbeb15cc94
ArG0rithm-Ace <zreset0@gmail.com>
2025-06-02 19:41:12 +0900

    更新

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
-  fetch("header.html")
+  fetch("include/header.html")
     .then(res => res.text())
     .then(html => document.body.insertAdjacentHTML("afterbegin", html));
 
-  fetch("footer.html")
+  fetch("include/footer/footer.html")
     .then(res => res.text())
     .then(html => document.body.insertAdjacentHTML("beforeend", html));
 });
+
+function particles(rootDir){
+  $.ajax({
+      url: rootDir + "include/particles/particles.html", // ディレクトリー変更
+      cache: false,
+      async: false,
+      dataType: 'html',
+      success: function(html){
+          html = html.replace(/\{\$root\}/g, rootDir); 
+          document.write(html);
+      }
+  });
+}
